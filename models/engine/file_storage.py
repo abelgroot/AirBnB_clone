16720c59c121f7ea2f2bff49103e0995e42f88cd
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-11-14 15:54:46 +0300

    pycodestyle for all files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,7 +13,8 @@ from models.user import User
 
 
 class FileStorage:
-    """Serializes instances to a JSON file and deserializes back to instances"""
+    """Serializes instances to a JSON file and
+    deserializes back to instances"""
 
     __file_path = "file.json"
     __objects = {}
