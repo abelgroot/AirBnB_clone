13bd3b290d364cda84107867266678f8d5ccddd9
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-11-16 17:48:25 +0300

    update base_model

--- models/base_model.py
+++ models/base_model.py
@@ -6,8 +6,6 @@ defines the BaseModel class that serves as the base class for other models.
 import uuid
 from datetime import datetime
 
-from models import storage
-
 
 class BaseModel:
     """Defines common attributes/methods for other classes."""
@@ -32,6 +30,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            from models import storage
+
             storage.new(self)
 
     def __str__(self):
@@ -41,6 +41,8 @@ class BaseModel:
     def save(self):
         """Updates the updated_at attribute with the current datetime."""
         self.updated_at = datetime.now()
+        from models import storage
+
         storage.save()  # Save the updated instance to storage
 
     def to_dict(self):
